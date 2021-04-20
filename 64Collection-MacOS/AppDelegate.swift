fbab7cb28838c1b95e3db24fa7ff59a2afea66a5
rhit-yangh6 <yangh6@rose-hulman.edu>
2021-04-20 12:45:16 +0800

    Initial Commit

---
+++ 64Collection-MacOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  64Collection-MacOS
+//
+//  Created by Hanyu Yang on 2021/4/20.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ 64Collection-MacOS/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  64Collection-MacOS
+//
+//  Created by Hanyu Yang on 2021/4/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
